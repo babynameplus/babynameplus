c6d61537d8fefaa84c3a478e4ea9ef6e6be14a3a
wojtasfi <wojciech.figas@outlook.com>
2017-04-11 20:54:42 +0200

    Postgresql configuration

--- src/main/java/com/babynameplus/BabynameplusApplication.java
+++ src/main/java/com/babynameplus/BabynameplusApplication.java
@@ -1,9 +1,15 @@
 package com.babynameplus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableAutoConfiguration
+@ComponentScan
 public class BabynameplusApplication {
 
 	public static void main(String[] args) {
