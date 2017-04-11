350c0d94a665810aab280730b225ad806bf5fbe7
wojtasfi <wojciech.figas@outlook.com>
2017-04-11 18:38:09 +0200

    Initial commit

---
+++ src/main/java/com/babynameplus/BabynameplusApplication.java
@@ -0,0 +1,12 @@
+package com.babynameplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabynameplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabynameplusApplication.class, args);
+	}
+}
