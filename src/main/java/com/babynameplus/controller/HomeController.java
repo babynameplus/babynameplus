bc0783f82bd4017e8f70689e1238efede812ab4b
Wojtek <babynameplus@gmail.com>
2017-06-03 12:37:58 +0200

    Add simple names fetching

--- src/main/java/com/babynameplus/controller/HomeController.java
+++ src/main/java/com/babynameplus/controller/HomeController.java
@@ -1,8 +1,17 @@
 package com.babynameplus.controller;
 
+import com.babynameplus.dto.NameDTO;
+import com.babynameplus.entities.Name;
+import com.babynameplus.enums.Sex;
+import com.babynameplus.service.NameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wojci on 4/11/2017.
@@ -12,9 +21,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private NameService nameService;
 
-    @RequestMapping(method= RequestMethod.GET)
-    public String home(){
-        return "home";
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model) {
+
+        List<Name> maleNames = nameService.fetchMaleNames();
+        List<Name> femaleNames = nameService.fetchFemaleNames();
+
+        model.addAttribute("maleNames", toNamesDto(maleNames));
+        model.addAttribute("femaleNames", toNamesDto(femaleNames));
+
+        return "names";
+    }
+
+    private List<NameDTO> toNamesDto(List<Name> names) {
+        List<NameDTO> dtoNames = new ArrayList<>();
+        names.forEach(name -> {
+            System.out.println(name.getName());
+            System.out.println(name.getSex());
+            NameDTO dto = new NameDTO();
+            dto.setName(name.getName());
+            String dtoSex;
+
+            if (name.getSex() == Sex.M) {
+                System.out.println("Man");
+                dtoSex = "Mężczyzna";
+            } else {
+                dtoSex = "Kobieta";
+            }
+
+            dto.setSex(dtoSex);
+            dtoNames.add(dto);
+        });
+        return dtoNames;
     }
 }
--- src/main/java/com/babynameplus/dao/NameRepository.java
+++ src/main/java/com/babynameplus/dao/NameRepository.java
@@ -1,8 +1,11 @@
 package com.babynameplus.dao;
 
 import com.babynameplus.entities.Name;
+import com.babynameplus.enums.Sex;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by wojci on 4/11/2017.
@@ -10,4 +13,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NameRepository extends CrudRepository<Name, Long> {
+
+    List<Name> findBySex(Sex sex);
 }
